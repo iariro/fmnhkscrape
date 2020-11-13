592cfb8818d98097bc13b11a7b88b529f875205c
kumagai <iariro@gol.com>
2020-11-14 05:33:14 +0900

    情報取得とHTML出力を分ける

--- fmnhkscrape.py
+++ fmnhkscrape.py
@@ -4,99 +4,118 @@ import datetime
 import re
 import urllib
 import urllib.parse
-import urllib.request as urllib2
+import urllib.request
 from bs4 import BeautifulSoup
 from ftplib import FTP
 
-def find_and_create_html(file):
-	headers = {
-			"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"
-			}
+def find_program(keywords):
+    user_agent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"
+    headers = {"User-Agent": user_agent}
 
-	nowyear = datetime.date.today().year
-	nowyear1 = "%s" % (nowyear)
-	nowyear2 = "%s" % (nowyear + 1)
+    nowyear = datetime.date.today().year
+    nowyear1 = "%s" % (nowyear)
+    nowyear2 = "%s" % (nowyear + 1)
 
-	file.write("<html>")
-	file.write("<head>")
-	file.write("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>")
-	file.write("<link rel='stylesheet' type='text/css' href='hatena.css'>")
-	file.write("</head>")
-	file.write("<body>")
-	file.write("<div class=hatena-body>")
-	file.write("<div class=main>")
+    all_results = []
+    for keyword in keywords:
+        # アクセスするURL
+        topurl = "https://www2.nhk.or.jp/hensei/program/query.cgi?f=kwd&area=001&qt=%s"
+        topurl = topurl % (urllib.parse.quote(keyword))
 
-	keywords = ["交響曲","受難曲","ドイツ・レクイエム","オネゲル","シェーンベルク","ストラヴィンスキー","ヒナステラ","ライヒ","ラヴェル","ジプシー","ジョエル","スキャッグス","ストラトヴァリウス","マルムスティーン","バルトーク","メシアン","ショスタコーヴィチ"]
-	for keyword in keywords:
-		# アクセスするURL
-		topurl = "https://www2.nhk.or.jp/hensei/program/query.cgi?f=kwd&area=001&qt=%s" % (urllib.parse.quote(keyword))
+        request = urllib.request.Request(topurl, headers=headers)
+        html = urllib.request.urlopen(request)
+        soup = BeautifulSoup(html, "html.parser")
+        alltd = soup.find_all('td')
 
-		request = urllib.request.Request(topurl, headers=headers)
-		html = urllib.request.urlopen(request)
-		soup = BeautifulSoup(html, "html.parser")
-		alltd = soup.find_all('td')
+        programurls = []
+        lastelement = None
+        for td in alltd:
+            for element in td.children:
+                if lastelement == "FM" and element.name == "a":
+                    programurls.append({"url": element.get("href"),
+                                        "title": "%s" % element.contents[0].replace("\xa0", "")})
+                lastelement = element
 
-		programurls = []
-		lastelement = None
-		for td in alltd:
-			for element in td.children:
-				if lastelement == "FM" and element.name == "a":
-					programurls.append({"url":element.get("href"), "title":"%s" % element.contents[0].replace("\xa0", "")})
-				lastelement = element
+        results = []
+        all_results.append({'keyword': keyword, 'result': results})
 
-		file.write("<h1>%s</h1>\n" % (keyword))
-		file.write("<div class=day>")
+        # 番組のループ
+        for programurl in programurls:
+            if '名曲スケッチ' in programurl["title"]:
+                continue
+            if '名曲の小箱' in programurl["title"]:
+                continue
 
-		# 番組のループ
-		for programurl in programurls:
-			if '名曲スケッチ' in programurl["title"]:
-				continue
-			if '名曲の小箱' in programurl["title"]:
-				continue
+            result = {'title': programurl["title"], 'text': []}
+            results.append(result)
+            programurl2 = "http://www2.nhk.or.jp/hensei/program/" + programurl["url"]
+            request2 = urllib.request.Request(programurl2, headers=headers)
+            html = urllib.request.urlopen(request2)
+            soup = BeautifulSoup(html, "html.parser")
 
-			file.write("<h2>%s</h2>\n" % (programurl["title"]))
-			request2 = urllib.request.Request("http://www2.nhk.or.jp/hensei/program/" + programurl["url"], headers=headers)
-			html = urllib.request.urlopen(request2)
-			soup = BeautifulSoup(html, "html.parser")
+            composition = []
 
-			composition = []
+            for dd in soup.find_all('dd'):
+                for element in dd.children:
+                    lines = ("%s" % (element)).split("<br/>")
+                    for line in lines:
+                        if nowyear1 in line or nowyear2 in line:
+                            result['text'].append(line.strip())
+                        else:
+                            if re.match("「.*", line) or "作曲" in line:
+                                composition.append(line)
 
-			for dd in soup.find_all('dd'):
-				for element in dd.children:
-					lines = ("%s" % (element)).split("<br/>")
-					for line in lines:
+                            if "作曲" in line:
+                                if len([line2 for line2 in composition if keyword in line2]) > 0:
+                                    for line2 in composition:
+                                        result['text'].append(line2)
+                                composition = []
+    return all_results
 
-						if nowyear1 in line or nowyear2 in line:
-							file.write("%s<br>\n" % (line.strip()))
-						else:
-							if re.match("「.*", line) or "作曲" in line:
-								composition.append(line)
+def create_html(all_results):
+    with open('fmnhk.html', mode='w') as file:
+        file.write("<html>")
+        file.write("<head>")
+        file.write("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>")
+        file.write("<link rel='stylesheet' type='text/css' href='hatena.css'>")
+        file.write("</head>")
+        file.write("<body>")
+        file.write("<div class=hatena-body>")
+        file.write("<div class=main>")
 
-							if "作曲" in line:
-								if len([line2 for line2 in composition if keyword in line2]) > 0:
-									for line2 in composition:
-										file.write("%s<br>\n" % (line2))
-								composition = []
-		file.write("</div>")
+        for results in all_results:
+            file.write("<h1>%s</h1>\n" % (results['keyword']))
+            file.write("<div class=day>")
+            for program in results['result']:
+                file.write("<h2>%s</h2>\n" % (program["title"]))
+                for line in program['text']:
+                    file.write("%s<br>\n" % (line))
+            file.write("</div>")
 
-	file.write("<br>")
-	file.write(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
-	file.write("<br>")
+        file.write("<br>")
+        file.write(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+        file.write("<br>")
 
-	file.write("</div>")
-	file.write("</div>")
-	file.write("</body>")
-	file.write("</html>")
+        file.write("</div>")
+        file.write("</div>")
+        file.write("</body>")
+        file.write("</html>")
+
 
 if __name__ == '__main__':
-	with open('fmnhk.html', mode='w') as file:
-		find_and_create_html(file)
+    keywords = ["交響曲", "受難曲", "ドイツ・レクイエム",
+                "オネゲル", "シェーンベルク", "ストラヴィンスキー", "ヒナステラ", "ライヒ", "ラヴェル",
+                "ジプシー", "ジョエル", "スキャッグス", "ストラトヴァリウス", "マルムスティーン",
+                "バルトーク", "メシアン", "ショスタコーヴィチ"]
 
-	FTP.encoding = "utf-8"
+    results = find_program(keywords)
+    create_html(results)
 
-	# FTP接続.
-	ftp = FTP("www2.gol.com", "ip0601170243", passwd="Z#5uqBpt")
+    FTP.encoding = "utf-8"
 
-	# ファイルのアップロード（テキスト）.
-	with open("fmnhk.html", "rb") as f:  # 注意：バイナリーモード(rb)で開く必要がある
-		ftp.storlines("STOR /private/web/hobby/fmnhk/fmnhk.html", f)
+    # FTP接続.
+    ftp = FTP("www2.gol.com", "ip0601170243", passwd="Z#5uqBpt")
+
+    # ファイルのアップロード（テキスト）.
+    with open("fmnhk.html", "rb") as f:  # 注意：バイナリーモード(rb)で開く必要がある
+        ftp.storlines("STOR /private/web/hobby/fmnhk/fmnhk.html", f)
